55c3c788b93ff6f398c6cd75b08cf88d97025da1
navidmojir <154021774+navidmojir@users.noreply.github.com>
2025-07-25 14:37:52 +0330

    .

--- src/main/java/ir/mojir/crud_generator/CrudGenerator.java
+++ src/main/java/ir/mojir/crud_generator/CrudGenerator.java
@@ -1,9 +1,27 @@
 package ir.mojir.crud_generator;
 
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+
 public class CrudGenerator {
 
-    public static void createSources(String basePackage) {
-        
+    public static void main(String[] args) {
+        try {
+            createSources("test");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createSources(String basePackage) throws URISyntaxException {
+        URL url = CrudGenerator.class.getClassLoader().getResource("templatefiles");
+
+        File templateFilesDir = Paths.get(url.toURI()).toFile();
+        for(File file: templateFilesDir.listFiles()) {
+            System.out.println(file.getName());
+        }
 
     }
 
